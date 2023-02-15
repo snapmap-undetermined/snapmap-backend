91cdea11cb4a5a00260b2c26b9d31bfb5f7e9324
shinkyuchul <kc960923@naver.com>
2023-02-15 13:42:02 +0900

    Feat : application.yml 설정 및 패키지 구조 세팅, HealthChecker 생성

---
+++ album/src/main/java/com/project/album/HealthChecker.java
@@ -0,0 +1,15 @@
+package com.project.album;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health-check")
+public class HealthChecker {
+
+    @GetMapping()
+    public String healthcheck() {
+        return "OK";
+    }
+}
