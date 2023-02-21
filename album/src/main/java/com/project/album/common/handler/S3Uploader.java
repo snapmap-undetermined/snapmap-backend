a192ae08e21f4d8c5a85a894dfb7626ae7ef0be4
shinkyuchul <kc960923@naver.com>
2023-02-21 11:11:39 +0900

    Feat : cloud front domain 추가

--- album/src/main/java/com/project/album/common/handler/S3Uploader.java
+++ album/src/main/java/com/project/album/common/handler/S3Uploader.java
@@ -24,6 +24,9 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class S3Uploader {
     private final AmazonS3Client amazonS3Client;
+    
+    public static final String CLOUD_FRONT_DOMAIN_NAME = "d3b7b3nw9cqnmc.cloudfront.net";
+
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
