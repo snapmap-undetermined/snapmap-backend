cb47c59f1260a3d3d6dc40a87b86b32763228c2f
sunghyeon <sunghyeon_jo@naver.com>
2023-04-09 21:00:36 +0900

    Update deploy.sh

--- project/scripts/deploy.sh
+++ project/scripts/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-BUILD_JAR=$(ls /home/ec2-user/action/build/libs/*.jar)
+BUILD_JAR=$(ls /home/ec2-user/action/build/libs/pinnit-0.0.1-SNAPSHOT.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 
 echo "> 현재 시간: $(date)" >> /home/ec2-user/action/deploy.log
 echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/action/deploy.log
-echo "> build 파일 복사" >> /home/ec2-user/action/deploy.log
 
+echo "> build 파일 복사" >> /home/ec2-user/action/deploy.log
 DEPLOY_PATH=/home/ec2-user/action/
 cp $BUILD_JAR $DEPLOY_PATH
 
@@ -17,11 +17,11 @@ if [ -z $CURRENT_PID ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/action/deploy.log
 else
-  echo "> kill -9 $CURRENT_PID" >> /home/ec2-user/action/deploy.log
-  sudo kill -9 $CURRENT_PID
+  echo "> kill -15 $CURRENT_PID" >> /home/ec2-user/action/deploy.log
+  sudo kill -15 $CURRENT_PID
   sleep 5
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포" >> /home/ec2-user/action/deploy.log
-sudo nohup java -jar $DEPLOY_JAR >/dev/null 2>&1 &
+echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/action/deploy.log
+sudo nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/action/deploy_err.log &
