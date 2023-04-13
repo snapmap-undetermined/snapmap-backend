7ddbc1f6a6aa30f5d3e9d7b31ea66d668eb6c0ce
shinkyuchul <kc960923@naver.com>
2023-04-13 19:40:40 +0900

    Feat : swagger 1차 작업

--- project/src/main/java/com/project/domain/comment/controller/PinCommentController.java
+++ project/src/main/java/com/project/domain/comment/controller/PinCommentController.java
@@ -7,7 +7,15 @@ import com.project.domain.comment.dto.PictureCommentDTO;
 import com.project.domain.comment.dto.PinCommentDTO;
 import com.project.domain.friend.dto.FriendDTO;
 import com.project.domain.pin.api.PinService;
+import com.project.domain.users.dto.UserDTO;
 import com.project.domain.users.entity.Users;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
@@ -16,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "핀 댓글 API", description = "Pin Comment Controller")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/pin-comment")
@@ -23,6 +32,10 @@ public class PinCommentController {
 
     private final PinCommentService pinCommentService;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "핀 댓글 생성 성공", content = @Content(schema = @Schema(implementation = PinCommentDTO.PinCommentDetailResponse.class)))})
+    @Operation(summary = "핀 댓글 생성", description = "핀 댓글을 생성한다.")
+    @SecurityRequirement(name = "Bearer Authentication")
     @PostMapping("")
     @Permission
     public ResponseEntity<PinCommentDTO.PinCommentDetailResponse> createPinComment(@AuthUser Users user, @RequestBody PinCommentDTO.CreatePinCommentRequest request) {
@@ -30,6 +43,10 @@ public class PinCommentController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "핀에 속한 핀 댓글 리스트 조회 성공", content = @Content(schema = @Schema(implementation = PinCommentDTO.PinCommentListResponse.class)))})
+    @Operation(summary = "핀 댓글 리스트 조회", description = "핀에 속한 핀 댓글 리스트를 조회 한다.")
+    @SecurityRequirement(name = "Bearer Authentication")
     @GetMapping("/{pinId}")
     @Permission
     public ResponseEntity<PinCommentDTO.PinCommentListResponse> getPinCommentByPinId(@PathVariable Long pinId) {
@@ -40,6 +57,10 @@ public class PinCommentController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "핀 댓글 삭제 성공", content = @Content(schema = @Schema(implementation = PinCommentDTO.PinCommentDetailResponse.class)))})
+    @Operation(summary = "핀 댓글 삭제", description = "핀 댓글을 삭제 한다.")
+    @SecurityRequirement(name = "Bearer Authentication")
     @DeleteMapping("/{pinCommentId}")
     @Permission
     public ResponseEntity<PinCommentDTO.PinCommentDetailResponse> deletePinComment(@PathVariable Long pinCommentId) {
@@ -48,6 +69,10 @@ public class PinCommentController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "핀 댓글 상태 삭제 성공", content = @Content(schema = @Schema(implementation = PinCommentDTO.PinCommentDetailResponse.class)))})
+    @Operation(summary = "핀 댓글 상태 삭제", description = "핀 댓글 상태 삭제를 한다.")
+    @SecurityRequirement(name = "Bearer Authentication")
     @DeleteMapping("/status/{pinCommentId}")
     @Permission
     public ResponseEntity<PinCommentDTO.PinCommentDetailResponse> deletePinCommentWithStatus(@PathVariable Long pinCommentId) {
@@ -56,6 +81,10 @@ public class PinCommentController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "핀 댓글 수정 성공", content = @Content(schema = @Schema(implementation = PinCommentDTO.PinCommentDetailResponse.class)))})
+    @Operation(summary = "핀 댓글 수정", description = "핀 댓글을 수정한다.")
+    @SecurityRequirement(name = "Bearer Authentication")
     @PatchMapping("/{pinCommentId}")
     @Permission
     public ResponseEntity<PinCommentDTO.PinCommentDetailResponse> updatePinComment(@PathVariable Long pinCommentId, @RequestBody PinCommentDTO.UpdatePinCommentRequest request) {
--- project/src/main/java/com/project/domain/users/controller/UserController.java
+++ project/src/main/java/com/project/domain/users/controller/UserController.java
@@ -3,10 +3,18 @@ package com.project.domain.users.controller;
 import com.project.common.annotation.AuthUser;
 import com.project.common.annotation.Permission;
 import com.project.domain.friend.dto.FriendDTO;
+import com.project.domain.pin.dto.PinDTO;
 import com.project.domain.users.api.interfaces.UserService;
 import com.project.domain.users.dto.UserDTO;
 import com.project.domain.users.api.interfaces.AuthService;
 import com.project.domain.users.entity.Users;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "유저 API", description = "User Controller")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
@@ -24,21 +33,28 @@ public class UserController {
     private final AuthService authService;
     private final UserService userService;
 
-    // 회원가입
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원가입 성공", content = @Content(schema = @Schema(implementation = UserDTO.SignUpResponse.class)))})
+    @Operation(summary = "회원가입", description = "회원가입을 한다.")
     @PostMapping("/signUp")
-    private ResponseEntity<UserDTO.SignUpResponse> signUp(@Valid @RequestBody UserDTO.SignUpRequest signUpRequest) throws Exception{
+    private ResponseEntity<UserDTO.SignUpResponse> signUp(@Valid @RequestBody UserDTO.SignUpRequest signUpRequest) throws Exception {
         UserDTO.SignUpResponse response = authService.signUp(signUpRequest);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    // 로그인
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "로그인 성공", content = @Content(schema = @Schema(implementation = UserDTO.LoginResponse.class)))})
+    @Operation(summary = "로그인", description = "로그인을 한다.")
     @PostMapping("/login")
     private ResponseEntity<UserDTO.LoginResponse> login(@Valid @RequestBody UserDTO.LoginRequest loginRequest) throws Exception {
         UserDTO.LoginResponse response = authService.login(loginRequest);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    // user 조회(검색)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "검색을 통한 유저 조회 성공", content = @Content(schema = @Schema(implementation = UserDTO.UserSimpleInfoResponse.class)))})
+    @Operation(summary = "유저 조회", description = "검색을 통해서 유저를 조회 한다.")
+    @SecurityRequirement(name = "Bearer Authentication")
     @GetMapping("")
     @Permission
     private ResponseEntity<UserDTO.UserSimpleInfoResponse> getUserByNickname(@RequestParam String userNickname) throws Exception {
@@ -48,13 +64,21 @@ public class UserController {
     }
 
     @PatchMapping("")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "유저 정보 수정 성공", content = @Content(schema = @Schema(implementation = UserDTO.UserSimpleInfoResponse.class)))})
+    @Operation(summary = "유저 수정", description = "유저의 정보를 수정한다.")
+    @SecurityRequirement(name = "Bearer Authentication")
     @Permission
-    private ResponseEntity<UserDTO.UserSimpleInfoResponse> updateUserNickname(@AuthUser Users user, @RequestBody  UserDTO.UpdateUserRequest request) {
+    private ResponseEntity<UserDTO.UserSimpleInfoResponse> updateUser(@AuthUser Users user, @RequestBody UserDTO.UpdateUserRequest request) {
         UserDTO.UserSimpleInfoResponse response = userService.updateUser(user.getId(), request);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @DeleteMapping("")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "유저 탈퇴 성공", content = @Content(schema = @Schema(implementation = void.class)))})
+    @Operation(summary = "유저 탈퇴", description = "유저가 탈퇴한다.")
+    @SecurityRequirement(name = "Bearer Authentication")
     @Permission
     private void deleteUser(@AuthUser Users user) {
         userService.deleteUser(user);
